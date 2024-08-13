d74a3d1faf4c3dd8ecee26ea696de5328fc08122
Kennyhunter254 <kipronokevin566@gmail.com>
2024-08-13 14:07:37 -0700

    react hooks props basic lab

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    </div>
+  );
+}
+
+export default Links;
